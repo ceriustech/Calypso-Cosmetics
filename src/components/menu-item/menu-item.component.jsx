ea93348a30ef171d9f4c10e31ad8c24da97e1468
Bill <ceriustech@gmail.com>
2020-08-30 12:26:17 -0500

    Finished styling the menu-item component.

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -3,14 +3,15 @@ import React from "react";
 import "./menu-item.styles.scss";
 
 const MenuItem = ({ title, imageUrl, linkUrl, linkText }) => (
-  <div
-    style={{
-      backgroundImage: `url(${imageUrl})`,
-    }}
-    className="menu-item"
-  >
+  <div className="menu-item">
+    <div
+      style={{
+        backgroundImage: `url(${imageUrl})`,
+      }}
+      className="background-image"
+    ></div>
     <div className="content">
-      <h1 className="title">{title}</h1>
+      <h1 className="title">{title.toUpperCase()}</h1>
       <span className="subtitle">{linkText}</span>
     </div>
   </div>
