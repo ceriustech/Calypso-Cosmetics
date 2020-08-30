c9e5e7f03b3f9dfa603bc823301b6d5952477b1c
Bill <ceriustech@gmail.com>
2020-08-30 12:59:21 -0500

    Created a banner component.

---
+++ src/components/banner/banner.component.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import "./banner.styles.scss";
+
+const Banner = () => (
+  <div className="banner">
+    <div className="banner-content">
+      <div className="banner-img_container">
+        <img src="" alt="some image" />
+      </div>
+      <div className="banner-text">
+        <h1>Welcome!</h1>
+        <h2>Thanks for shopping with us.</h2>
+      </div>
+    </div>
+  </div>
+);
+
+export default Banner;
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 
 import Directory from "../../components/directory/directory.component";
+import Banner from "../../components/banner/banner.component";
 
 import "./homepage.styles.scss";
 
 const HomePage = () => (
   <div className="homepage">
+    <Banner />
     <Directory />
   </div>
 );
