08fedcb8c6f30d2241e3ea865da42670344e3dda
Bill <ceriustech@gmail.com>
2020-09-01 19:43:09 -0500

    Added header component and logo image.

---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import Logo from "../../assets/calypso-cosmetics-logo-img.jpeg";
+
+import "./header.styles.scss";
+
+const Header = () => (
+  <div className="header">
+    <Link to="/">
+      <div className="logo-container">
+        <img src={Logo} alt="calypso cosmetics" />
+      </div>
+    </Link>
+    <div className="options">
+      <Link className="option" to="/shop">
+        SHOP
+      </Link>
+      <Link className="option" to="/shop">
+        CONTACT
+      </Link>
+    </div>
+  </div>
+);
+
+export default Header;
