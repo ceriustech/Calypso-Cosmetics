c3eb5daa08a07e1e220f329f2796849c92b96150
Bill <ceriustech@gmail.com>
2020-09-28 20:05:04 -0500

    Added cart functionality to app with cart-icon and cart-dropdown components with redux.

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,12 +4,13 @@ import { connect } from "react-redux";
 import { auth } from "../../firebase/firebase.utils";
 
 import CartIcon from "../cart-icon/cart-icon.component";
+import CartDropdown from "../cart-dropdown/cart-dropdown.component";
 
 import Logo from "../../assets/calypso-cosmetics-logo.jpg";
 
 import "./header.styles.scss";
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
   <div className="header">
     <Link to="/">
       <div className="logo-container">
@@ -34,11 +35,13 @@ const Header = ({ currentUser }) => (
       )}
       <CartIcon />
     </div>
+    {hidden ? null : <CartDropdown />}
   </div>
 );
 
-const mapStateToProps = (state) => ({
-  currentUser: state.user.currentUser,
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+  currentUser,
+  hidden,
 });
 
 export default connect(mapStateToProps)(Header);
