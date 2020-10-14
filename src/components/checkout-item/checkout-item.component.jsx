bae3881c23e9e9b84787cccfa247c09b0c06bb5b
Bill <ceriustech@gmail.com>
2020-10-13 22:15:04 -0500

    Created checkout page and checkout item component.

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 
 import './checkout-item.styles.scss'
 
-const CheckoutItem = () => {
+const CheckoutItem = ({cartItem: {name, imageUrl, price, quantity}}) => {
     return (
-        <div className="checkou-item">
+        <div className="checkout-item">
             <div className="image-container">
-
+                <img src={imageUrl} alt="item" />
             </div>
+            <p className="name">{name}</p>
+            <p className="quantity">{quantity}</p>
+            <p className="price">{price}</p>
+            <div className="remove-button">&#x58;</div>
         </div>
     )
 }
